3c6f4c0fdb26a7261e3a5c9e61c5302a2e869b4b
mohamad-karimi <karimi.mohamad0011@gmail.com>
2026-08-22 16:08:15 +0330

    Add open meto api weather and template and use cache

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -7,6 +7,9 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from .filters import TasksFilter
 from .paginations import CustomPagination
 from .permissions import IsOwnerOrReadOnly
+import requests
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class TaskModelViewSet(viewsets.ModelViewSet):
@@ -38,3 +41,34 @@ class TaskModelViewSet(viewsets.ModelViewSet):
         """
 
         serializer.save(user=self.request.user)
+
+
+class WeatherAPIView(APIView):
+    def get(self, request):
+        response = requests.get(
+            "https://api.open-meteo.com/v1/forecast",
+            params={
+                "latitude": 35.8400,
+                "longitude": 50.9391,
+                "current": (
+                    "temperature_2m,"
+                    "relative_humidity_2m,"
+                    "weather_code,"
+                    "wind_speed_10m"
+                ),
+                "timezone": "Asia/Tehran",
+            },
+            timeout=10,
+        )
+
+        response.raise_for_status()
+        data = response.json()
+
+        weather_data = {
+            "temperature": data["current"]["temperature_2m"],
+            "humidity": data["current"]["relative_humidity_2m"],
+            "weather_code": data["current"]["weather_code"],
+            "wind_speed": data["current"]["wind_speed_10m"],
+        }
+
+        return Response(weather_data)
